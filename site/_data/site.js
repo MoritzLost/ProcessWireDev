77e0592b4a8936e11f3fa1e0be76c75d52227dd8
Moritz L'Hoest <info@herebedragons.world>
2020-10-09 16:47:04 +0200

    Add optional env-based URL override

--- site/_data/site.js
+++ site/_data/site.js
@@ -3,7 +3,7 @@ const package = require('../../package.json');
 module.exports = () => {
     return {
         name: "processwire.dev",
-        url: package.homepage,
+        url: process.env.SITE_URL || package.homepage,
         version: package.version,
         lang: "en",
         dir: "ltr"
