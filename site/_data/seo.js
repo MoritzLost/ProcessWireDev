854c28d700c20efd8c3320b38b54a029851fec9c
Moritz L'Hoest <info@herebedragons.world>
2020-10-12 17:31:39 +0200

    Add twitter metadata

---
+++ site/_data/seo.js
@@ -0,0 +1,9 @@
+module.exports = () => {
+    return {
+        twitter: {
+            card: 'summary_large_image',
+            site: false,
+            creator: '@MoritzLost',
+        }
+    }
+}
